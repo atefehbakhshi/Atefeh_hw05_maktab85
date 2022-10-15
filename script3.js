d4f56141e8b16c81fb3feb1863fee84c7a7c90fc
Atefeh Bakhshi <at.bakhshi1373@gmail.com>
2022-10-15 16:12:23 +0330

    adding question 3

---
+++ script3.js
@@ -0,0 +1,19 @@
+let user = [
+    {
+        name: 'Eli',
+        age: 32
+    },
+    {
+        name: 'Reza',
+        age: 23
+    },
+    {
+        name: 'Ati',
+        age: 25
+    },
+
+];
+const sortingByAge = (obj) => {
+    return obj.sort((a, b) => a.age - b.age)
+}
+console.log(sortingByAge(user))
