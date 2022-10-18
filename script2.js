92d7d5e6c3aa096118e09ab0dd12a95e04fa0010
Atefeh Bakhshi <at.bakhshi1373@gmail.com>
2022-10-18 08:55:46 +0330

    modifying excesive p which had display id

--- script2.js
+++ script2.js
@@ -3,24 +3,23 @@ const counterStep = {
     step: 0,
     increase: function () {
         this.step++;
-        return this;
+        return this
     },
     decrease: function () {
         this.step--;
-        return this;
+        return this
     },
     reset: function () {
         this.step = 0;
-        return this;
+        return this
     },
     read: function () {
         const pTag = document.createElement("p");
         pTag.innerHTML = this.step;
-        const display = document.getElementById('display')
-        document.body.insertBefore(pTag, display)
-        return this;
+        document.body.appendChild(pTag);
+        return this
     },
 }
 
-counterStep.read().increase().read().increase().reset().increase().decrease().decrease().read().reset().decrease().read().increase().read();
+counterStep.increase().read().increase().reset().increase().decrease().decrease().read().reset().decrease().read().increase().read();
 
