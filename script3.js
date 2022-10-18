ea54ea4d1148448169856d93e460b620e2ee473a
Atefeh Bakhshi <at.bakhshi1373@gmail.com>
2022-10-18 10:42:23 +0330

    adding style to show result in html

--- script3.js
+++ script3.js
@@ -5,6 +5,10 @@ let user = [
         age: 32
     },
     {
+        name: 'Mohammad',
+        age: 8
+    },
+    {
         name: 'Reza',
         age: 23
     },
@@ -12,9 +16,40 @@ let user = [
         name: 'Ati',
         age: 25
     },
+    {
+        name: 'Eli',
+        age: 5
+    },
 
 ];
 const sortingByAge = (obj) => {
-    return obj.sort((a, b) => a.age - b.age)
+    const sortedtObject = obj.sort((a, b) => a.age - b.age);
+    resultTable(sortedtObject);
 }
-console.log(sortingByAge(user))
+// display result in html
+const resultTable = (obj) => {
+    const table = document.createElement('table');
+
+    // the header of result table
+    const tableHeaderItems = ['Item', 'Name', 'Age'];
+    let headerRow = table.insertRow();
+    tableHeaderItems.forEach(item => {
+        let headerCell = document.createElement("th");
+        headerCell.innerHTML = item;
+        headerRow.appendChild(headerCell);
+    });
+
+    // the body of result table
+    for (let i = 0; i < obj.length; i++) {
+        const row = table.insertRow();
+        const item = row.insertCell(0)
+        const userName = row.insertCell(1)
+        const age = row.insertCell(2)
+        item.innerHTML = i + 1;
+        userName.innerHTML = obj[Object.keys(obj)[i]].name;
+        age.innerHTML = obj[Object.keys(obj)[i]].age;
+        document.body.appendChild(table)
+    }
+}
+
+sortingByAge(user);
